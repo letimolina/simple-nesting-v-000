70c247e75e43a3f55c5bf7070b160311da2ccde7
Leticia Molina <letimolina@gmail.com>
2018-05-27 21:00:02 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     returns programmer_hash[:dennis_ritchie][:languages]
+     returns programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
